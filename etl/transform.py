b4ff282e5f5ecb8d0458f04e3b8f9321b052a34f
Mickael Darmon <mickael.darmon@live.fr>
2026-07-16 13:58:10 +0200

    Fichier de transformation des données

---
+++ etl/transform.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+
+def merge_sources(hr, sport):
+
+    df = hr.merge(
+        sport,
+        on="ID salarié",
+        how="left"
+    )
+
+    return df
+
+
+def fill_missing_sports(df):
+
+    df["Pratique d'un sport"] = (
+        df["Pratique d'un sport"]
+        .fillna("Aucune")
+    )
+
+    return df
+
+
+def normalize_transport(df):
+
+    mapping = {
+        "Marche/running": "Marche",
+        "Running": "Marche",
+        "Course à pied": "Marche",
+        "A pied": "Marche",
+
+        "Vélo": "Vélo",
+        "Trottinette": "Trottinette",
+
+        "Voiture": "Voiture",
+        "Transport en commun": "Transport en commun"
+    }
+
+    df["Moyen de déplacement"] = (
+        df["Moyen de déplacement"]
+        .replace(mapping)
+        .str.strip()
+    )
+
+    return df
